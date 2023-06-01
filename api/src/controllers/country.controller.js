1f547a1ec26b17d32be4967b2ffd3c24449694c8
AlanLuna-00 <alan.luna.desarrollo@gmail.com>
2023-06-01 15:53:26 -0300

    aaa

--- api/src/controllers/country.controller.js
+++ api/src/controllers/country.controller.js
@@ -1,35 +1,40 @@
-const { getCountriesApi, searchCountryByName, getCountryById } = require('../services/country.services');
-const { Country, Activity } = require('../db.js');
+const {
+  getCountriesApi,
+  searchCountryByName,
+  getCountryById,
+} = require("../services/country.services");
+const { Country, Activity } = require("../db.js");
 
 const getCountries = async (req, res) => {
-    const { name } = req.query;
-    try {
-        if (name) {
-            const country = await searchCountryByName(name);
-            res.status(200).json(country);
-        } else {
-            const countries = await getCountriesApi();
-            res.status(200).json(countries);
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Error getting countries' });
+  const { name } = req.query;
+  try {
+    if (name) {
+      const country = await searchCountryByName(name);
+      res.status(200).json(country);
+    } else {
+      const countries = await getCountriesApi();
+      res.status(200).json(countries);
+      console.log(countries);
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: "Error getting countries" });
+  }
+};
 
 // get country by id
 const getCountryByIdController = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const country = await getCountryById(id);
-        country ? res.status(200).json(country) : res.status(404).json({ message: 'Country not found' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error getting country' });
-    }
-}
-
-
+  const { id } = req.params;
+  try {
+    const country = await getCountryById(id);
+    country
+      ? res.status(200).json(country)
+      : res.status(404).json({ message: "Country not found" });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting country" });
+  }
+};
 
 module.exports = {
-    getCountries,
-    getCountryByIdController,
+  getCountries,
+  getCountryByIdController,
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
-const countriesRouter = require('./countriesRoutes.js')
-const activitiesRouter = require('./activitiesRoutes.js')
-
+const countriesRouter = require("./countriesRoutes.js");
+const activitiesRouter = require("./activitiesRoutes.js");
 
 const router = Router();
 
-// Configurar los routers
-router.use('/countries', countriesRouter);
-router.use('/activities', activitiesRouter);
+router.get("/", (req, res) => {
+  res.send("Welcome to Countries API");
+});
 
+// Configurar los routers
+router.use("/countries", countriesRouter);
+router.use("/activities", activitiesRouter);
 
 module.exports = router;
